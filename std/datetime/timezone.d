eb79925a5aebd30fd43b87c3cc3eb9e2fd817471
Joakim <git@joakim.fea.st>
2018-04-21 13:02:40 +0530

    Android: fix regression after scoping imports in std.datetime.timezone

--- std/datetime/timezone.d
+++ std/datetime/timezone.d
@@ -2808,6 +2808,7 @@ private:
             initOnce!_tzIndex(
             {
                 import std.conv : to;
+                import std.datetime.date : DateTimeException;
                 import std.format : format;
                 import std.path : asNormalizedPath, chainPath;
 
